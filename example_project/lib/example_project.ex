d6c8e1bc6308895c4880fcf0e0b7cea66b5cc123
damianobacci <damiano.bacci@gmail.com>
2025-11-22 23:27:25 +0100

    feat: added another function that returns atom

--- example_project/lib/example_project.ex
+++ example_project/lib/example_project.ex
@@ -3,6 +3,11 @@ defmodule ExampleProject do
 
   def start(_type, _args) do
     IO.puts("ExampleProject started")
+    IO.puts(ExampleProject.hello())
     Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def hello do
+    :world
   end
 end
