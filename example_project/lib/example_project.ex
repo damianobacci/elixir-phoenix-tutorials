8559c9f81c2a14d475716944230686c664f5d611
damianobacci <damiano.bacci@gmail.com>
2025-11-22 23:36:11 +0100

    feat: added uuid package from hex as an example

--- example_project/lib/example_project.ex
+++ example_project/lib/example_project.ex
@@ -1,9 +1,11 @@
 defmodule ExampleProject do
   use Application
+  alias UUID
 
   def start(_type, _args) do
     IO.puts("ExampleProject started")
     IO.puts(ExampleProject.hello())
+    IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
