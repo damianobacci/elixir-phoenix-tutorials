a9a2839e3dacc6b2441792d95a359a5946862132
damianobacci <damiano.bacci@gmail.com>
2025-11-23 18:06:53 +0100

    feat: add basic variables and module attributes

--- example_project/lib/example_project.ex
+++ example_project/lib/example_project.ex
@@ -2,14 +2,17 @@ defmodule ExampleProject do
   use Application
   alias UUID
 
+  @x 5  # Module attribute
+
   def start(_type, _args) do
     IO.puts("ExampleProject started")
-    IO.puts(ExampleProject.hello())
+    IO.puts(ExampleProject.main())
     IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  def hello do
-    :world
+  def main do
+    y = 10 # local variable
+    @x + y * 2
   end
 end
